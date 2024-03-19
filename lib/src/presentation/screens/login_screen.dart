4c3fe7a3a5956272a79786d78b304b8302e9f953
sagorsohel <sohel19sagor@gmail.com>
2024-03-20 05:44:40 +0600

    add project

--- lib/src/presentation/screens/login_screen.dart
+++ lib/src/presentation/screens/login_screen.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -7,6 +6,9 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('app'),
+      ),
     );
   }
 }
