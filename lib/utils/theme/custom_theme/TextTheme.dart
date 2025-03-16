0bad70ed758b1ca18d702f948f5bbc1e4786a1fa
sagorsohel <sohel19sagor@gmail.com>
2025-03-17 00:48:58 +0600

    Add text styles for light and dark themes in CustomTextTheme

--- lib/utils/theme/custom_theme/TextTheme.dart
+++ lib/utils/theme/custom_theme/TextTheme.dart
@@ -15,6 +15,48 @@ class CustomTextTheme {
       fontWeight: FontWeight.w600,
       fontFamily: 'Poppins',
     ),
+    titleLarge: const TextStyle().copyWith(
+      fontSize: 16.0,
+      fontWeight: FontWeight.w600,
+      color: Colors.black,
+    ),
+    titleMedium: const TextStyle().copyWith(
+      fontSize: 16.0,
+      fontWeight: FontWeight.w500,
+      color: Colors.black,
+    ),
+    titleSmall: const TextStyle().copyWith(
+      fontSize: 16.0,
+      fontWeight: FontWeight.w400,
+      color: Colors.black,
+    ),
+
+    bodyLarge: const TextStyle().copyWith(
+      fontSize: 14.0,
+      fontWeight: FontWeight.w500,
+      color: Colors.black,
+    ),
+    bodyMedium: const TextStyle().copyWith(
+      fontSize: 14.0,
+      fontWeight: FontWeight.normal,
+      color: Colors.black,
+    ),
+    bodySmall: const TextStyle().copyWith(
+      fontSize: 14.0,
+      fontWeight: FontWeight.w500,
+      color: Colors.black.withOpacity(0.5),
+    ),
+
+    labelLarge: const TextStyle().copyWith(
+      fontSize: 12.0,
+      fontWeight: FontWeight.normal,
+      color: Colors.black,
+    ),
+    labelMedium: const TextStyle().copyWith(
+      fontSize: 12.0,
+      fontWeight: FontWeight.normal,
+      color: Colors.black.withOpacity(0.5),
+    ),
   );
   static TextTheme darkTextTheme = TextTheme(
     headlineLarge: TextStyle().copyWith(
@@ -28,6 +70,48 @@ class CustomTextTheme {
       fontSize: 24,
       fontWeight: FontWeight.w600,
       fontFamily: 'Poppins',
+    ),
+    titleLarge: const TextStyle().copyWith(
+      fontSize: 16.0,
+      fontWeight: FontWeight.w600,
+      color: Colors.white,
+    ),
+    titleMedium: const TextStyle().copyWith(
+      fontSize: 16.0,
+      fontWeight: FontWeight.w500,
+      color: Colors.white,
+    ),
+    titleSmall: const TextStyle().copyWith(
+      fontSize: 16.0,
+      fontWeight: FontWeight.w400,
+      color: Colors.white,
+    ),
+
+    bodyLarge: const TextStyle().copyWith(
+      fontSize: 14.0,
+      fontWeight: FontWeight.w500,
+      color: Colors.white,
+    ),
+    bodyMedium: const TextStyle().copyWith(
+      fontSize: 14.0,
+      fontWeight: FontWeight.normal,
+      color: Colors.white,
+    ),
+    bodySmall: const TextStyle().copyWith(
+      fontSize: 14.0,
+      fontWeight: FontWeight.w500,
+      color: Colors.white.withOpacity(0.5),
+    ),
+
+    labelLarge: const TextStyle().copyWith(
+      fontSize: 12.0,
+      fontWeight: FontWeight.normal,
+      color: Colors.white,
+    ),
+    labelMedium: const TextStyle().copyWith(
+      fontSize: 12.0,
+      fontWeight: FontWeight.normal,
+      color: Colors.white.withOpacity(0.5),
     ),
   );
 }
